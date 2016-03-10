3b9656979d50c6ae2f3a1d549141ef47706202f9
Vaidas Jablonskis <jablonskis@gmail.com>
2016-03-10 15:00:37 +0000

    WIP

--- run.sh
+++ run.sh
@@ -1,27 +1,50 @@
 #!/usr/bin/bash
 
-: ${LS_HOME:=/var/lib/logstash}
-: ${HOME:=${LS_HOME}}
+export HOME=/var/lib/logstash
+
 : ${LS_HEAP_SIZE:=500m}
-: ${LS_JAVA_OPTS:=-Djava.io.tmpdir=${LS_HOME}}
-: ${LS_LOG_DIR:=/var/log/logstash}
-: ${OUTPUT_CLOUDWATCH:=true}
+: ${LS_JAVA_OPTS:=-Djava.io.tmpdir=${HOME}}
+: ${LS_LOG_DIR:=/var/lib/logstash}
+: ${LS_OPEN_FILES:=8192}
+
+: ${INPUT_JOURNALD:=true}
+
+: ${OUTPUT_CLOUDWATCH:=false}
 : ${AWS_REGION:=eu-west-1}
 : ${LOG_GROUP_NAME:=logstash}
 : ${LOG_STREAM_NAME:=$(hostname)}
 
-sed -e "s/%AWS_REGION%/${AWS_REGION}/" \
-    -e "s/%LOG_GROUP_NAME%/${LOG_GROUP_NAME}/" \
-    -e "s/%LOG_STREAM_NAME%/${LOG_STREAM_NAME}/" \
-    -i /etc/logstash/conf.d/20_output_kubernetes_cloudwatch.conf \
-    -i /etc/logstash/conf.d/20_output_journald_cloudwatch.conf
+: ${OUTPUT_ELASTICSEARCH:=true}
+: ${ELASTICSEARCH_HOST:=127.0.0.1:9200}
 
-if [[ ${OUTPUT_CLOUDWATCH} != 'true' ]]; then
-  rm -f /etc/logstash/conf.d/20_output_kubernetes_cloudwatch.conf
-  rm -f /etc/logstash/conf.d/20_output_journald_cloudwatch.conf
+
+if [[ ${INPUT_JOURNALD} != 'true' ]]; then
+  rm -f /logstash/conf.d/10_input_journald.conf
 fi
 
-ulimit -n ${LS_OPEN_FILES} > /dev/null
-cd ${LS_HOME}
 
-/opt/logstash/bin/logstash -f /etc/logstash/conf.d
+if [[ ${OUTPUT_ELASTICSEARCH} != 'true' ]]; then
+  rm -f /logstash/conf.d/20_output_journald_elasticsearch.conf
+  rm -f /logstash/conf.d/20_output_kubernetes_elasticsearch.conf
+else
+  sed -e "s/%ELASTICSEARCH_HOST%/${ELASTICSEARCH_HOST}/" \
+      -i /logstash/conf.d/20_output_kubernetes_elasticsearch.conf \
+      -i /logstash/conf.d/20_output_journald_elasticsearch.conf
+fi
+
+
+if [[ ${OUTPUT_CLOUDWATCH} != 'true' ]]; then
+  rm -f /logstash/conf.d/20_output_kubernetes_cloudwatch.conf
+  rm -f /logstash/conf.d/20_output_journald_cloudwatch.conf
+else
+  sed -e "s/%AWS_REGION%/${AWS_REGION}/" \
+      -e "s/%LOG_GROUP_NAME%/${LOG_GROUP_NAME}/" \
+      -e "s/%LOG_STREAM_NAME%/${LOG_STREAM_NAME}/" \
+      -i /logstash/conf.d/20_output_kubernetes_cloudwatch.conf \
+      -i /logstash/conf.d/20_output_journald_cloudwatch.conf
+fi
+
+
+ulimit -n ${LS_OPEN_FILES} > /dev/null
+
+/logstash/bin/logstash -f /logstash/conf.d
